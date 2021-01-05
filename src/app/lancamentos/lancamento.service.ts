32a16dc83b55825282c44168b60e707f7f32a9f8
João Victor <joaovictor.cogomes@gmail.com>
2021-01-04 21:04:42 -0300

    Filtro de lancamentos por data e inclusao de paginacao lazy

--- src/app/lancamentos/lancamento.service.ts
+++ src/app/lancamentos/lancamento.service.ts
@@ -1,8 +1,14 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-export interface LancamentoFiltro {
+import { format } from 'date-fns';
+
+export class LancamentoFiltro {
     descricao: string;
+    dataVencimentoInicio: Date;
+    dataVencimentoFim: Date;
+    pagina = 0;
+    itensPorPagina = 3;
 }
 
 @Injectable({
@@ -20,12 +26,36 @@ export class LancamentoService {
 
         let params = new HttpParams();
 
+        params = params.set('page', filtro.pagina.toString());
+        params = params.set('size', filtro.itensPorPagina.toString());
+
         if (filtro.descricao) {
             params = params.set('descricao', filtro.descricao);
         }
 
+        if (filtro.dataVencimentoInicio) {
+            params = params.set('dataVencimentoDe',
+                        format(filtro.dataVencimentoInicio, 'yyyy-MM-dd'));
+            console.log(format(filtro.dataVencimentoInicio, 'yyyy-MM-dd'));
+        }
+
+        if (filtro.dataVencimentoFim) {
+            params = params.set('dataVencimentoAte',
+                        format(filtro.dataVencimentoFim, 'yyyy-MM-dd'));
+            console.log(format(filtro.dataVencimentoFim, 'yyyy-MM-dd'));
+        }
+
+
         return this.http.get(`${this.lancamentosUrl}?resumo`, { headers, params })
             .toPromise()
-            .then(response => response['content'] );
+            .then(response => {
+                const lancamentos = response['content'];
+                const resultado = {
+                    lancamentos,
+                    total: response['totalElements']
+                }
+
+                return resultado;
+            });
     }
 }
--- src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { LancamentoService } from '../lancamento.service';
+import { LazyLoadEvent } from 'primeng/api';
+import { LancamentoFiltro, LancamentoService } from '../lancamento.service';
 
 @Component({
   selector: 'app-lancamentos-pesquisa',
@@ -8,18 +9,32 @@ import { LancamentoService } from '../lancamento.service';
 })
 export class LancamentosPesquisaComponent implements OnInit {
 
-    descricao: string;
+    totalRegistros = 0;
+    filtro = new LancamentoFiltro();
+
     lancamentos = [];
 
     constructor(private lancamentoService: LancamentoService) {}
 
     ngOnInit() {
-        this.pesquisar();
     }
 
-    pesquisar() {
-        this.lancamentoService.pesquisar({ descricao: this.descricao })
-            .then(lancamentos => this.lancamentos = lancamentos);
+    pesquisar(pagina = 0) {
+        this.filtro.pagina = pagina;
+
+        this.lancamentoService.pesquisar(this.filtro)
+            .then(resultado => {
+                this.totalRegistros = resultado.total;
+                this.lancamentos = resultado.lancamentos;
+            });
+    }
+
+    aoMudarPagina(event: LazyLoadEvent) {
+        const pagina = event.first / event.rows;
+
+        if (pagina != this.filtro.pagina) {
+            this.pesquisar(pagina);
+        }
     }
 
 }
