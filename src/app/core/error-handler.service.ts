3d990d8bf15ec7cbf581d88c16df652def1a4572
João Victor <joaovictor.cogomes@gmail.com>
2021-01-06 07:03:30 -0300

    Adicionando erro de retorno da API para o usuario

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CoreModule } from './core.module';
+import { HttpErrorResponse } from '@angular/common/http';
 
 import { MessageService } from 'primeng/api';
 
@@ -8,18 +9,27 @@ import { MessageService } from 'primeng/api';
 })
 export class ErrorHandlerService {
 
-  constructor(private messageService: MessageService) { }
+    constructor(private messageService: MessageService) { }
 
-  handle(errorResponse: any) {
-      let msg: string;
+    handle(errorResponse: any) {
+        let msg: string;
 
-      if (typeof errorResponse === 'string') {
-          msg = errorResponse;
-      } else {
-          msg = 'Erro ao processar o serviço remoto. Tente novamente.';
-          console.log('Ocorreu um erro', errorResponse);
-      }
+        if (typeof errorResponse === 'string') {
+            msg = errorResponse;
+        } else if (errorResponse instanceof HttpErrorResponse &&
+                    errorResponse.status >= 400 && errorResponse.status <= 499) {
+            msg = 'Ocorreu um erro ao processar a sua solicitação';
 
-      this.messageService.add({ severity: 'error', detail: msg });
-  }
+            try {
+                msg = errorResponse.error[0].mensagemUsuario;
+            } catch (e) { }
+
+            console.error('Ocorreu um erro', errorResponse);
+        } else {
+            msg = 'Erro ao processar o serviço remoto. Tente novamente.';
+            console.log('Ocorreu um erro', errorResponse);
+        }
+
+        this.messageService.add({ severity: 'error', detail: msg });
+    }
 }
