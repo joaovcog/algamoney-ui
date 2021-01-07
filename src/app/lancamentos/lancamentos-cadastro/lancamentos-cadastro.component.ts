43d4eca1bd2783d3d5dbad8ff0f631ab1fb00699
João Victor <joaovictor.cogomes@gmail.com>
2021-01-07 06:41:43 -0300

    Carregando a lista de pessoas no cadastro de lancamento

--- src/app/lancamentos/lancamentos-cadastro/lancamentos-cadastro.component.ts
+++ src/app/lancamentos/lancamentos-cadastro/lancamentos-cadastro.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { CategoriaService } from 'src/app/categorias/categoria.service';
 import { ErrorHandlerService } from 'src/app/core/error-handler.service';
+import { PessoaService } from 'src/app/pessoas/pessoa.service';
 
 @Component({
   selector: 'app-lancamentos-cadastro',
@@ -21,19 +22,30 @@ export class LancamentosCadastroComponent implements OnInit {
 
     constructor(
         private categoriaService: CategoriaService,
+        private pessoaService: PessoaService,
         private errorHandler: ErrorHandlerService
     ) { }
 
     ngOnInit(): void {
         this.carregarCategorias();
+        this.carregarPessoas();
     }
 
     carregarCategorias() {
-        return this.categoriaService.listarTodas()
+        this.categoriaService.listarTodas()
             .then(categorias => {
                 this.categorias = categorias.map(c => {
                     return { label: c.nome, value: c.codigo };
                 });
+            })
+            .catch(erro => this.errorHandler.handle(erro));
+    }
+
+    carregarPessoas() {
+        this.pessoaService.listarTodas()
+            .then(pessoas => {
+                this.pessoas = pessoas
+                    .map(p => ({ label: p.nome, value: p.codigo }));
             })
             .catch(erro => this.errorHandler.handle(erro));
     }
