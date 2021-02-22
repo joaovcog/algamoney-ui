4b774be0726188044aff9d11025168a7bc9ad25e
João Victor <joaovictor.cogomes@gmail.com>
2021-02-22 17:17:23 -0300

    Criacao do modulo e componente de relatorio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { PaginaNaoEncontradaComponent } from "./core/pagina-nao-encontrada.component";
 import { NaoAutorizadoComponent } from "./core/nao-autorizado.component";
+import { RelatoriosModule } from "./relatorios/relatorios.module";
 
 const routes: Routes = [
     { path: 'lancamentos', loadChildren: () => import('./lancamentos/lancamentos.module').then(m => m.LancamentosModule) },
     { path: 'pessoas', loadChildren: () => import('./pessoas/pessoas.module').then(m => m.PessoasModule) },
     { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+    { path: 'relatorios', loadChildren: () => import('./relatorios/relatorios.module').then(m => m.RelatoriosModule) },
 
     { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     { path: 'nao-autorizado', component: NaoAutorizadoComponent },
